7680ebdbd192296a96792bb540f30c09f584d407
walterDelivan <147879811+walterDelivan@users.noreply.github.com>
2026-07-01 21:36:35 -0300

    fix port

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ Reglas:
     res.status(500).json({ respuesta: "Error del servidor" });
   }
 });
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
